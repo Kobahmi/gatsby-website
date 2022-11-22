12c01ab553ee5c8cbcb000caf2ac528b4d527ff0
Nobel <nobel_lamun@hotmail.com>
2022-11-22 02:02:39 -0330

    added react icons library

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,19 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import { FiMenu } from "@react-icons/all-files/fi/FiMenu";
+import { menuData } from "../data/MenuData";
 
 const Header = () => {
-  return <h1>Header</h1>;
+  return (
+    <div className="bg-indigo-200 p-6">
+      <FiMenu className="h-10 w-10" />
+      {menuData.map((item, index) => (
+        <div key={index} to={item.link}>
+          {item.title}
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,9 @@ import Seo from "../components/seo";
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1 className="text-3xl font-bold underline">Hi people</h1>
+    <h1 className="text-3xl font-bold underline">
+      Hi people with Tailwind CSS
+    </h1>
   </Layout>
 );
 
